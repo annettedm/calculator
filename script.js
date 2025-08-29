829253a6976efa107532b8543729d7ad1f28b2f4
Annette Dm <51707930+annettedm@users.noreply.github.com>
2025-08-29 14:08:03 +0300

    AddSubstractMultiplyDivideFns

---
+++ script.js
@@ -0,0 +1,6 @@
+import { add, divide, multiply, subtract } from "./modules/basic_functions.js";
+
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
